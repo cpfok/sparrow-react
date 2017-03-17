0ac7d4eef6c25a5ed26864874d50f4f470d5b3de
pengfei.cai <pengfei.cai@chinaredstar.com>
2017-03-17 10:03:22 +0800

    升级使用rstool-build

--- webpack.config.js
+++ webpack.config.js
@@ -55,30 +55,29 @@ function getEntries(globPath, exclude) {
 var entries = getEntries('src/modules/*/index.js', /common/);
 
 var config = {
-  entry: entries,
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: configFile.outputJs,
-    publicPath: '@dummy_domain@',
-    chunkFilename: "[name].js"
-    //libraryTarget: 'umd'
-  },
+  //entry: entries,
+  //output: {
+  //  path: path.join(__dirname, 'dist'),
+  //  filename: configFile.outputJs,
+  //  publicPath: '@dummy_domain@',
+  //  chunkFilename: "[name].js"
+  //  //libraryTarget: 'umd'
+  //},
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({
-      "process.env": {
-        // This has effect on the react lib size
-        "NODE_ENV": JSON.stringify('production')
-      },
-      "env": options.env
-    }),
-    new HtmlWebpackPlugin({
-      hash: false,
-      template: 'src/template.html'
-    }),
+    //new webpack.DefinePlugin({
+    //  "process.env": {
+    //    // This has effect on the react lib size
+    //    "NODE_ENV": JSON.stringify('production')
+    //  },
+    //  "env": options.env
+    //}),
+    //new HtmlWebpackPlugin({
+    //  hash: false,
+    //  template: 'src/template.html'
+    //}),
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'common',
-      filename: configFile.commonJs
+      name: 'common'
     }),
     cssExtractor
   ],
@@ -92,59 +91,41 @@ var config = {
     clientLogLevel: 'info',
     open: true,
     proxy: {
-      '/api-uc': {
-        changeOrigin: true,
-        target: 'http://localhost:3000',
-        secure: false,
-        //pathRewrite: {'^/api': ''}
-      },
       '/api': {
         changeOrigin: true,
         target: 'http://localhost:3000',
         secure: false,
         //pathRewrite: {'^/api': ''}
-      },
-      '/ueditor': {
-        changeOrigin: true,
-        target: 'http://localhost:3000/',
-        secure: false,
-        pathRewrite: {'^/api': ''}
       }
     }
-  },
-  //resolve: {
-  //  modules: ['node_modules', path.join(__dirname, '../node_modules')],
-  //  extensions: ['.web.tsx', '.web.ts', '.web.jsx', '.web.js', '.ts', '.tsx', '.js', '.jsx', '.json'],
-  //},
+  }
 };
 
-Object.keys(entries).forEach(function (name) {
-  // 每个页面生成一个entry，如果需要HotUpdate，在这里修改entry
-  config.entry[name] = entries[name];
-  let file = entries[name];
-  let filePath = file.split(path.sep);
-  let curdir = __dirname.split(path.sep);
-  curdir.pop();
-  var configFile = path.join(__dirname, 'src', 'modules', name, 'config.json');
-  let perConfig = require(configFile);
-  // 每个页面生成一个html
-  var plugin = new HtmlWebpackPlugin({
-    // 生成出来的html文件名
-    filename: name + '.html',
-    // 每个html的模版，这里多个页面使用同一个模版
-    template: path.join(__dirname, 'src', 'template.html'),
-    // 自动将引用插入html
-    inject: 'body',
-    title: perConfig.html.title,
-    // 每个html引用的js模块，也可以在这里加上vendor等公用模块
-    chunks: ['common', name]
-  });
-  config.plugins.push(plugin);
-})
-//config.plugins.push(new HelloWorldPlugin({options: true}));
-//config.plugins.push(new StringReplacePlugin());
+//Object.keys(entries).forEach(function (name) {
+//  // 每个页面生成一个entry，如果需要HotUpdate，在这里修改entry
+//  config.entry[name] = entries[name];
+//  let file = entries[name];
+//  let filePath = file.split(path.sep);
+//  let curdir = __dirname.split(path.sep);
+//  curdir.pop();
+//  var configFile = path.join(__dirname, 'src', 'modules', name, 'config.json');
+//  let perConfig = require(configFile);
+//  // 每个页面生成一个html
+//  var plugin = new HtmlWebpackPlugin({
+//    // 生成出来的html文件名
+//    filename: name + '.html',
+//    // 每个html的模版，这里多个页面使用同一个模版
+//    template: path.join(__dirname, 'src', 'template.html'),
+//    // 自动将引用插入html
+//    inject: 'body',
+//    title: perConfig.html.title,
+//    // 每个html引用的js模块，也可以在这里加上vendor等公用模块
+//    chunks: ['common', name]
+//  });
+//  //config.plugins.push(plugin);
+//})
 module.exports = (webpackConfig)=> {
-  config.plugins = webpackConfig.plugins.concat(config.plugins);
+  //config.plugins = webpackConfig.plugins.concat(config.plugins);
   let retVal = Object.assign({}, webpackConfig, config);
-  return retVal;
+  return webpackConfig;
 };
